e67dea6029c9be65c78a8beb07555a1adf780410
Hassan Porter <90788434+GoReala23@users.noreply.github.com>
2025-09-30 12:42:48 -0400

    chore(core): stabilize config and stub feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,17 @@
 // ==========================================================
 
 import { Module } from '@nestjs/common';
-import {ConfigModule} from '@nestjs/config';
-import {MongooseModule} from '@nestjs/mongoose';
+// import {ConfigModule} from '@nestjs/config';
+// import {MongooseModule} from '@nestjs/mongoose';
 import {AppController} from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     // Load .env globally
-    ConfigModule.forRoot({ isGlobal: true}),
+    // ConfigModule.forRoot({ isGlobal: true}),
     // Connect to MongoDB
-    MongooseModule.forRoot(process.env.MONGO_URI || ''),
+    // MongooseModule.forRoot(process.env.MONGO_URI || ''),
 
     // Future modules will go here (Auth, Users, Avatars, etc)
   ],
